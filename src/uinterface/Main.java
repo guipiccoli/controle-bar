4fc97a8f74b7e06b26d0a98e09faf4c0970f945b
Gabriel Weich <gabrielweich@hotmail.com>
2018-04-12 23:28:50 -0300

    Estrutura básica para criação da interface

--- src/uinterface/Main.java
+++ src/uinterface/Main.java
@@ -1,10 +1,37 @@
 package uinterface;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Gesiel");
+		launch(args);
 	}
+	
+	@Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 
 }
